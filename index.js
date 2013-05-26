4827ea6fd895add3087f3cc189139ba626bd220e
Hans Huebner <hans.huebner@gmail.com>
2013-05-26 23:07:22 +0200

    basic socket.io scaffolding

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var path = require('path');
+var fs = require('fs');
+var events = require('events');
+var util = require('util');
+var http = require('http');
+var express = require('express');
+var notemplate = require('express-notemplate');
 var SerialPort = require('serialport2').SerialPort;
 var port = new SerialPort();
 
@@ -80,3 +87,37 @@ port.open(deviceName, {
     console.log('serial port open');
 });
 
+var app = express();
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + '/uploads' }));
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('blikdiblu'));
+    app.use(express.session());
+    app.use(app.router);
+    app.set('statics', process.cwd() + '/public');
+    app.engine('html', notemplate.__express);
+    app.set('view engine', 'html');
+    app.use(express.static(app.get('statics')));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+})
+
+server.listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
