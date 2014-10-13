a8a7bf3beb55266b55ebb2fe9e6c256ad2bfd6e8
Hans Huebner <hans.huebner@gmail.com>
2014-10-13 02:35:35 +0200

    updates nach dem gleisdreieck-rennen, neue berichte

--- load.lisp
+++ load.lisp
@@ -1,5 +1,5 @@
 ;; -*- Lisp -*-
 
-(ql:quickload '(:yason :local-time :cl-ppcre :cl-postgres+local-time :postmodern))
+(ql:quickload '(:yason :local-time :cl-ppcre :cl-postgres+local-time :postmodern :xhtmlgen :alexandria))
 
 (load (compile-file "load-race-log.lisp"))
